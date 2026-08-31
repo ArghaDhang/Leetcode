b51432c7b25ff51bffdd0b2370430866c8a2d604
Argha Dhang <arghadhang@gmail.com>
2026-08-31 23:16:29 +0530

    Time: 3 ms (68.11%) | Memory: 16 MB (25.43%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void backtrack(string curr, int open, int close, int n) {
+
+        // Complete valid combination
+        if (curr.size() == 2 * n) {
+            ans.push_back(curr);
+            return;
+        }
+
+        // Add '('
+        if (open < n) {
+            backtrack(curr + '(', open + 1, close, n);
+        }
+
+        // Add ')'
+        if (close < open) {
+            backtrack(curr + ')', open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return ans;
+    }
+};
